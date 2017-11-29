e28b9f4b8839f39cd427ebbc8d8be76e51f7d557
Adam Jensen <oojensen@alumni.stanford.edu>
2017-11-29 10:46:25 -0800

    remove floyd_requirements, try longer script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-echo 'PATH:'
+#unused alternative: ln -s YOURS_PATH_TO_cuDNN/libcudnn.so.7.0.3 YOURS_PATH_TO_cuDNN/libcudnn.so.6
+
+echo 'PATH is:'
 echo $PATH
-
-echo 'LD_LIBRARY_PATH'
+echo 'LD_LIBRARY_PATH is:'
 echo $LD_LIBRARY_PATH
+echo "lib64:"
+ls /usr/local/cuda/lib64/ | grep "libcudn*"
+echo "include: "
+ls /usr/local/cuda/include/ | grep "libcudn*"
 
-echo "lib64"
-ls /usr/local/cuda/lib64/ | grep "libcud*"
+wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE} -P /root/downloads && \
+cd /root/downloads && \
+tar -xzvf ${CUDNN_TAR_FILE} && \
+cp cuda/include/cudnn.h /usr/local/cuda-8.0/include && \
+cp cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/ && \
+chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
 
-echo "include"
-ls /usr/local/cuda/include/ | grep "libcud*"
+export CUDA_HOME=/usr/local/cuda
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64
 
-#ln -s YOURS_PATH_TO_cuDNN/libcudnn.so.7.0.3 YOURS_PATH_TO_cuDNN/libcudnn.so.6
+cd /usr/local/cuda/lib64 && \
+rm libcudnn.so && \
+rm libcudnn.so.6 && \
+ln libcudnn.so.6.* libcudnn.so.6 && \
+ln libcudnn.so.6 libcudnn.so && \
+ldconfig
+
+echo 'PATH is:'
+echo $PATH
+echo 'LD_LIBRARY_PATH is:'
+echo $LD_LIBRARY_PATH
+echo "lib64:"
+ls /usr/local/cuda/lib64/ | grep "libcudn*"
+echo "include: "
+ls /usr/local/cuda/include/ | grep "libcudn*"
+
+pip3 install tf-nightly-gpu
