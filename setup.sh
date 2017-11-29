4db44cb83af8cb06709258a0cc84eee9cc2ce851
Adam Jensen <oojensen@alumni.stanford.edu>
2017-11-29 10:28:47 -0800

    twrk

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ echo 'LD_LIBRARY_PATH'
 echo $LD_LIBRARY_PATH
 
 echo "ls /usr/local/cuda/lib64/libcu*"
-ls /usr/local/cuda/lib64/libcu*
+ls /usr/local/cuda/lib64/ | grep "libcud*"
 
 echo "ls /usr/local/cuda/include/libcu*"
-ls /usr/local/cuda/include/libcu*
+ls /usr/local/cuda/include/ | grep "libcud*"
 
 #ln -s YOURS_PATH_TO_cuDNN/libcudnn.so.7.0.3 YOURS_PATH_TO_cuDNN/libcudnn.so.6
